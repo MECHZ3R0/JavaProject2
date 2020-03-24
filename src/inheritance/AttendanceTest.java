d60ba06dc8fe458d66734d5d904a04b19c1e2fb1
Umedi <urasulzoda@na.edu>
2020-03-24 15:05:55 -0600

    Mar 24, 2020

--- src/inheritance/AttendanceTest.java
+++ src/inheritance/AttendanceTest.java
@@ -2,11 +2,13 @@ package inheritance;
 
 public class AttendanceTest {
     public static void main(String[] args) {
-        String[] names={"Ab","Cd","Ef","Gh","Ij"};
+        String[] names={"","Siri","Alexa","Cortana","Google","Bixby"};
         int[] id = {12,34,56,78,90};
         Attendance x = new Attendance(names,id);
         x.enterAttendance();
-        x.printAttendance(x.enterAttendance());
+        x.printWeeklyAttendance(x.enterAttendance());
+        x.printMostAttendedPerson(x.enterAttendance());
+        x.printMostAttndedDay(x.enterAttendance());
     }
 
 }
